bb45d4510cc7fc03042e6bbb5f3fcc0901b35d6b
Jed Hastwell <jed@whistlefire.com>
2018-08-10 12:11:30 +0200

    🛠 Data values for mine shaft worker and crate

--- src/game/values.js
+++ src/game/values.js
@@ -4,6 +4,10 @@ const values = {
 
   transportTime: 1.5,
   transportPause: 1,
+  minerWorkTime: 2,
+  minerWalkTime: 1.5,
+
+  crateUnloadTime: 0.6,
 
   set: function (obj) {
     util.extend(this, obj);
