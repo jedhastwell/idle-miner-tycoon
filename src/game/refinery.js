ef9aa88a37bbf3ebf23d202ea9bd2c6e87574636
Jed Hastwell <jed@whistlefire.com>
2018-08-10 10:41:06 +0200

    🛠 Push refinary animation into a method

--- src/game/refinery.js
+++ src/game/refinery.js
@@ -2,6 +2,7 @@ import * as Pixi from 'pixi.js';
 import {Sprite, Texture, Container} from 'pixi.js';
 import Core  from '../core.js';
 import Anims from './animations.js';
+import values from './values';
 
 
 class Refinery extends Container {
@@ -17,11 +18,19 @@ class Refinery extends Container {
     roof.position.set(0, building.y - roof.height);
     this.addChild(roof);
 
-    // const pipe = makeAnimatedSprite(pipeAnim);
-    const pipe = Anims.make(Anims.pipe);
+    const pipe = this._pipe = Anims.make(Anims.pipe);
     pipe.position.set(roof.width, roof.y + roof.height * 0.4);4
     this.addChild(pipe);
+
   }
+
+  unload () {
+    this._pipe.play(0);
+    Core.engine.wait(values.transportPause).then(() => {
+      this._pipe.gotoAndStop(0);
+    });
+  }
+
 
 }
 
